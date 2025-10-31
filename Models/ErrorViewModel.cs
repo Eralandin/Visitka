10dbd1057a4d1388553b9f8a909202bbf92bb8fe
Eralandin <eriktt2005teuhreb@mail.ru>
2025-10-31 18:32:00 +0700

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Visitka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
