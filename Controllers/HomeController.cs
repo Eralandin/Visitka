3b889f19277267a3050fab5655e72f76b0c549fb
Lericon <supercolor2013@gmail.com>
2025-10-31 22:07:50 +0700

    Доделаны header и footer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Visitka.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
