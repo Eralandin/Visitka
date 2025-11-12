5e11712cd8b029b6664081c2234eb4e3c61642cb
Lericon <supercolor2013@gmail.com>
2025-11-12 19:34:24 +0700

    Исправление "Контакты", адаптация главной страницы

--- wwwroot/js/contact.js
+++ wwwroot/js/contact.js
@@ -5,12 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
         item.addEventListener('click', function() {
             const index = this.getAttribute('data-index');
             
-            // Закрываем все картинки
             imageItems.forEach(img => {
                 img.classList.remove('active');
             });
             
-            // Открываем выбранную картинку
             this.classList.add('active');
         });
     });
